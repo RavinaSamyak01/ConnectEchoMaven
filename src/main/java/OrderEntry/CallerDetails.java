94c51fd0006b42e6cf33ed1f6da77dd7facb144c
Ravina Prajapati <ravina.prajapati.samyak@gmail.com>
2022-01-06 15:02:23 +0530

    6th Jan 2022

---
+++ src/main/java/OrderEntry/CallerDetails.java
@@ -0,0 +1,14 @@
+package OrderEntry;
+
+import org.testng.annotations.Test;
+
+import samyak_BasePackage.BaseInit;
+
+public class CallerDetails extends BaseInit {
+
+	@Test
+	public void callerDetails() {
+		
+		
+	}
+}
